e7e95f1d58406f01ba06bdca44acf6a56cff191b
Crab <crab@quantoptiongroup.com>
2023-09-05 21:03:19 -0700

    Updates

---
+++ precache-manifest.f1b7b622443d51030de645f3bdb7eb6d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5403f6b3c22226b4752596c244225a60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71a8185fbfcf3ae71c43",
+    "url": "/static/css/2.f01b737b.chunk.css"
+  },
+  {
+    "revision": "d75dd76af9ffadb37a3c",
+    "url": "/static/css/main.5e554192.chunk.css"
+  },
+  {
+    "revision": "71a8185fbfcf3ae71c43",
+    "url": "/static/js/2.2dc38a17.chunk.js"
+  },
+  {
+    "revision": "82085174ba6ed216ecb09c7a11d8bf17",
+    "url": "/static/js/2.2dc38a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75dd76af9ffadb37a3c",
+    "url": "/static/js/main.da8f6f1c.chunk.js"
+  },
+  {
+    "revision": "04b2f78c2e282ee1297a",
+    "url": "/static/js/runtime-main.76317cd4.js"
+  },
+  {
+    "revision": "4355078f6a948669ba3cf70045ac0a4b",
+    "url": "/static/media/alert.4355078f.mp3"
+  },
+  {
+    "revision": "5ce15f800623e6e13ede276ea73ed067",
+    "url": "/static/media/logo.5ce15f80.svg"
+  }
+]);
